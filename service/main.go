f22312fc864b656fbca9d393217a5b20b6a53484
Shuping <shuping@pdx.edu>
2018-01-09 12:24:06 -0800

    change IP

--- service/main.go
+++ service/main.go
@@ -24,7 +24,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.107.101:9200"
+	ES_URL = "http://35.227.30.159:9200"
 )
 
 type Post struct {
